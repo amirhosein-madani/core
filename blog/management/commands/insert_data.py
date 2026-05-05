2ff713b278b2382caea35185f20d951b225e88f4
amir <amirmadani901@gmail.com>
2026-05-05 20:19:35 +0330

    adding custom comand

---
+++ blog/management/commands/insert_data.py
@@ -0,0 +1,42 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+from accounts.models import User, Profile
+from ...models import Post, Category
+from random import choice
+
+
+class Command(BaseCommand):
+    help = "creating random users and posts"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+
+        categories = list(Category.objects.all())
+
+        user = User.objects.create_user(
+            username=self.fake.user_name(),
+            password=self.fake.password(),
+            phone_number=self.fake.phone_number(),
+            email=self.fake.email(),
+        )
+
+        profile = Profile.objects.get(user=user)
+        profile.first_name = self.fake.first_name()
+        profile.last_name = self.fake.last_name()
+        profile.date_of_birth = self.fake.date_of_birth()
+        profile.description = self.fake.paragraph(nb_sentences=5)
+        profile.save()
+
+        for _ in range(10):
+            post = Post.objects.create(
+                author=profile,
+                title=self.fake.text(),
+                content=self.fake.paragraph(nb_sentences=10),
+                status=choice([True, False]),
+                published_date=self.fake.date_time_this_year(),
+            )
+
+            post.category.add(choice(categories))
