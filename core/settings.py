01edc178b2470ad5002dc767d101edd7973334d4
amir <amirmadani901@gmail.com>
2026-06-25 13:04:23 +0330

    changes in setting

--- core/settings.py
+++ core/settings.py
@@ -33,9 +33,9 @@ ALLOWED_HOSTS = config(
 # email configuration
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp4dev"
-EMAIL_PORT = 25
-EMAIL_USE_TLS = False
+EMAIL_HOST = config("EMAIL_HOST", default="smtp4dev")
+EMAIL_PORT = config("EMAIL_PORT", cast=int, default=25)
+EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool, default=False)
 EMAIL_HOST_USER = ""
 EMAIL_HOST_PASSWORD = ""
 
